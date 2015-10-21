989d655fcf0006d4416aa4ef53ec0bedbfebb08a
meezl <meezl@10.18.151.26>
2015-10-21 10:00:03 -0600

    10/21/2015 - Control Classes

---
+++ dungeonTester/src/byui/cit260/dungeonTester/control/CombatControl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.dungeonTester.control;
+
+/**
+ *
+ * @author meezl
+ */
+public class CombatControl {
+
+    public double playerAttack(double attackStat, double enemyDefense, double enemyHealth) {
+
+        if (enemyHealth <= 0 || enemyHealth > 99) {
+            return -1;
+        }
+
+        if (attackStat < 1 || attackStat > 99) {
+            return -1;
+        }
+
+        if (enemyDefense < 1 || enemyDefense > 99) {
+            return -1;
+        }
+        if (attackStat > enemyDefense) {
+            double damageRating = attackStat – enemyDefense;
+            double damageDealt = enemyHealth – damageRating;
+            return damageDealt;
+        } 
+        
+        else {
+            return 0;
+        }
+    }
