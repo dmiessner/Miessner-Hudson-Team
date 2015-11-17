730fe3b406a4cb291c4a25019654a8cdbceddef8
Meez <Meez@MSI>
2015-11-16 22:09:52 -0700

    11/16/2015 - Inheritance Backpack View

--- dungeonTester/src/byui/cit260/dungeonTester/view/BackpackView.java
+++ dungeonTester/src/byui/cit260/dungeonTester/view/BackpackView.java
@@ -5,79 +5,51 @@
  */
 package byui.cit260.dungeonTester.view;
 
-import java.util.Scanner;
-
 /**
  *
  * @author meezl
  */
-public class BackpackView {
+public class BackpackView extends View {
 
-    private final String MENU = "\n"
-            + "\n==============================================="
-            + "\n| Backpack                                   |"
-            + "\n==============================================="
-            + "\nI - View items"
-            + "\nU - Use items"
-            + "\nW - Wear items"
-            + "\nE - Exit"
-            + "\n===============================================";
+    public BackpackView() {
 
-    void displayMenu() {
-        char selection = ' ';
-        do {
-
-            System.out.println(MENU); // Displays the main menu
-
-            String input = this.getInput(); // Get the user's input
-            selection = input.charAt(0); // Get the first character of the string
-
-            this.doAction(selection); // Do an action based on the user's selection 
-        } while (selection != 'E'); // Any selection that is not exit will cause a loop
+        super("\n"
+                + "\n==============================================="
+                + "\n| Backpack                                   |"
+                + "\n==============================================="
+                + "\nI - View items"
+                + "\nU - Use items"
+                + "\nW - Wear items"
+                + "\nE - Exit"
+                + "\n===============================================");
     }
 
-    private String getInput() {
-        boolean valid = false; // indicates that the input still needs to be retrieved.  
-        String input = null;
-        Scanner keyboard = new Scanner(System.in);
+    @Override
+    public boolean doAction(Object input) {
 
-        while (!valid) { // a valid name has yet to be retrieved
+        String value = (String) input;
 
-            // prompt for the player's name
-            System.out.println("Enter your input");
+        value = value.toUpperCase();
+        char choice = value.charAt(0);
 
-            // get the name from the keyboard, trim off blanks.
-            input = keyboard.nextLine();
-            input = input.trim();
-
-            // if the players name is less than 2 characters in length then this happens
-            if (input.length() > 1) {
-                System.out.println("Invalid input - the input must be G, H, S, or E, and only one character");
-                continue;
-            }
-            break; // exit the repetition
-        }
-        return input; // return the input
-    }
-
-    private void doAction(char selection) {
-        switch (selection) {
-            case 'I': // Create a new game
-                this.viewItems();
+        switch (choice) {
+            case 'W': // Create a new game
+                this.wearItems();
                 break;
             case 'U': // get and start an existing game
                 this.useItems();
                 break;
-            case 'W': // display the help menu
-                this.wearItems();
+            case 'I': // display the help menu
+                this.viewItems();
                 break;
             case 'E': // exit the program
-                return;
+                return true;
             default:
                 System.out.println("\n*** Invalid selection, try again ***");
                 break;
-        }
 
+        }
+        return false;
     }
 
     private void wearItems() {
